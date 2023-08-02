695f4fe223c851bdc8e56d28dfa870b689f04ac1
RyosukeSomeya <ryosuke.someya.0618.dev@gmail.com>
2023-08-03 01:36:42 +0900

    insta page追加

--- instagram.php
+++ instagram.php
@@ -6,10 +6,16 @@ Template Name: INSTAGRAM
 <?php get_header(); ?>
 <main class="l-container">
   <section class="p-instagram">
-    <h1 class="c-section-heading--center" data-animate="fI">FOLLOW US ON<br>INSTAGRAM</h1>
-    <section>
-      <?php // echo do_shortcode('[instagram-feed num=30 cols=3 showheader=false showfollow=false]'); ?>
-    </section>
+    <h1 class="c-heading-section c-heading-section--center">
+      FOLLOW US ON<br />
+      INSTAGRAM
+    </h1>
+    <div class="p-instagram__icon c-icon--instagram">
+      <img src="<?php bloginfo('template_url'); ?>/assets/images/insta_icon.png" alt="" />
+    </div>
+    <div class="p-instagram__feed-container">
+      <?php echo do_shortcode('[instagram-feed feed=2]')?>
+    </div>
   </section>
 </main>
 <?php get_footer(); ?>
