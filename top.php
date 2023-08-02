ba90959489f60d710c56d13517ab0108615a2e6c
RyosukeSomeya <ryosuke.someya.0618.dev@gmail.com>
2023-08-03 00:34:21 +0900

    スライダー画像をカスタムフィールドから設定できるように修正

--- top.php
+++ top.php
@@ -4,14 +4,48 @@ Template Name: top
 */
 ?>
 <?php get_header(); ?>
+<?php
+$page_obj = get_page_by_path('top');
+$page_id = $page_obj->ID;
+$custom_keys = get_post_custom_keys($page_id);
+$img_keys = [];
+$img_imgs = [];
+// スライダー画像のPCとSPの組数を算出するための配列を作成
+foreach ($custom_keys as $key) {
+  if (preg_match('/^(pc|sp)_slider_img/', $key)) {
+    // keyの数字を取得
+    $num = (int)substr($key, -1);
+    // 配列にまだない場合は追加する
+    if (!in_array($num, $img_keys)) {
+      array_push($img_keys, $num);
+    }
+  }
+}
+// 画像の組数
+$img_pare_count = count($img_keys);
+// リンクを格納
+foreach ($custom_keys as $key) {
+  if (preg_match('/^(pc|sp)_slider_img/', $key)) {
+    $img = [$key => get_field($key)];
+    $img_imgs = array_merge($img_imgs, $img);
+  }
+}
+
+?>
 
 <main>
   <section class="c-slider">
+    <!-- スライダー部 -->
     <ul class="slider full">
-      <li><img class="pc" src="<?php bloginfo('template_url'); ?>/assets/images/slide1.png" alt="" /><img class="sp" src="<?php bloginfo('template_url'); ?>/assets/images/slide1_sp.png" alt="" /></li>
-      <li><img class="pc" src="<?php bloginfo('template_url'); ?>/assets/images/slide2.png" alt="" /><img class="sp" src="<?php bloginfo('template_url'); ?>/assets/images/slide2_sp.png" alt="" /></li>
-      <li><img class="pc" src="<?php bloginfo('template_url'); ?>/assets/images/slide3.png" alt="" /><img class="sp" src="<?php bloginfo('template_url'); ?>/assets/images/slide3_sp.png" alt="" /></li>
-      <li><img class="pc" src="<?php bloginfo('template_url'); ?>/assets/images/slide4.png" alt="" /><img class="sp" src="<?php bloginfo('template_url'); ?>/assets/images/slide4_sp.png" alt="" /></li>
+      <?php for ($i = 0; $i < $img_pare_count; $i++) : ?>
+        <li>
+          <?php
+          foreach ($img_imgs as $key => $img) : if (preg_match("/" . $i + 1 . "/", $key) == 1) : ?>
+              <img class="<?php echo preg_match('/pc/', $key) == 1 ? 'pc' : 'sp' ?>" src="<?php echo $img ?>" alt="" />
+          <?php endif;
+          endforeach; ?>
+        </li>
+      <?php endfor; ?>
     </ul>
   </section>
   <section class="p-top-introduction l-container">
@@ -58,7 +92,7 @@ Template Name: top
       <div class="p-top-kv-section__kv-container-inner l-container l-container--wide">
         <section class="p-top-kv-section__card p-top-kv-section__card">
           <h2 class="p-top-kv-section__card-title c-heading-section--small">ECOALA AI Pro Style</h2>
-          <p class="p-top-kv-section__card-txt c-text--20">
+          <p class="p-top-kv-section__card-txt c-txt--20">
             わたし<span class="u-narrow-text">に</span>「いい<span class="u-narrow-text">」</span>が、いちば<span class="u-narrow-text">ん</span>「いい<span class="u-narrow-text">」</span>。<br>
             未だかつてない次世代ドライヤーを目指しました。
           </p>
@@ -85,11 +119,11 @@ Template Name: top
             </div>
           </h2>
           <section class="p-top-kv-section__card p-top-kv-section__card--ai-prostyle p-top-kv-section__card--right">
-            <h3 class="p-top-kv-section__card-title c-text--30">
+            <h3 class="p-top-kv-section__card-title c-txt--30">
               わたし<span class="u-narrow-text--mid">に</span>「いい<span class="u-narrow-text--mid">」</span>が<span class="u-narrow-text--mid">、</span><br>
               いちば<span class="u-narrow-text--mid">ん</span>「いい<span class="u-narrow-text--mid">」</span>。
             </h3>
-            <p class="p-top-kv-section__card-txt c-text--20">
+            <p class="p-top-kv-section__card-txt c-txt--20">
               髪は一生もの。だから最適なヘアドライを届けたい。<br>
               あなたの髪質・地肌に合わせた<br>
               あなただけのドライヤーが誕生しました。
@@ -108,11 +142,11 @@ Template Name: top
           <img src="<?php bloginfo('template_url'); ?>/assets/images/thumb1.png" alt="">
         </div>
         <dl class="p-top-feature-section__card-body">
-          <dt class="p-top-feature-section__card-title c-text--30">
+          <dt class="p-top-feature-section__card-title c-txt--30">
             高性能・高機能<br>
             プロ推奨ドライヤー
           </dt>
-          <dd class="p-top-feature-section__card-text c-text--20">
+          <dd class="p-top-feature-section__card-text c-txt--20">
             家庭用ドライヤーの域を超えた、プロの美容師も使いやすい機能を搭載。
           </dd>
           <dd>
@@ -128,11 +162,11 @@ Template Name: top
           <img src="<?php bloginfo('template_url'); ?>/assets/images/thumb2.png" alt="">
         </div>
         <dl class="p-top-feature-section__card-body">
-          <dt class="p-top-feature-section__card-title c-text--30">
+          <dt class="p-top-feature-section__card-title c-txt--30">
             世界に認められた<br>
             高いデザイン性
           </dt>
-          <dd class="p-top-feature-section__card-text c-text--20">
+          <dd class="p-top-feature-section__card-text c-txt--20">
             誰でも使いやすいように考え抜かれたUI。モダンで上質なデザインに。
           </dd>
           <dd>
@@ -148,11 +182,11 @@ Template Name: top
           <img src="<?php bloginfo('template_url'); ?>/assets/images/thumb3.png" alt="">
         </div>
         <dl class="p-top-feature-section__card-body">
-          <dt class="p-top-feature-section__card-title c-text--30">
+          <dt class="p-top-feature-section__card-title c-txt--30">
             富士山溶岩配合の<br>
             セラミックノズル
           </dt>
-          <dd class="p-top-feature-section__card-text c-text--20">
+          <dd class="p-top-feature-section__card-text c-txt--20">
             マイナスイオンと遠赤外線で、髪と頭皮にうるおいをもたらす。
           </dd>
           <dd>
@@ -168,11 +202,11 @@ Template Name: top
           <img src="<?php bloginfo('template_url'); ?>/assets/images/thumb4.png" alt="">
         </div>
         <dl class="p-top-feature-section__card-body">
-          <dt class="p-top-feature-section__card-title c-text--30">
+          <dt class="p-top-feature-section__card-title c-txt--30">
             スペック＆<br>
             安心のまごころ保証
           </dt>
-          <dd class="p-top-feature-section__card-text c-text--20">
+          <dd class="p-top-feature-section__card-text c-txt--20">
             万が一のトラブルに備えた安心保証を全てのお客様にご提供いたします。
           </dd>
           <dd>
@@ -195,11 +229,11 @@ Template Name: top
             </div>
           </h2>
           <section class="p-top-kv-section__card p-top-kv-section__card--dryer p-top-kv-section__card--right">
-            <h3 class="p-top-kv-section__card-title c-text--30">
+            <h3 class="p-top-kv-section__card-title c-txt--30">
               ファミリーでも ひとりでも、<br>
               髪と地肌に合わせたヘアケアを。
             </h3>
-            <p class="p-top-kv-section__card-txt c-text--20">
+            <p class="p-top-kv-section__card-txt c-txt--20">
               ノズルを付け替えれば、<br>
               それぞれの髪と地肌に適したドライヤーに。<br>
               気分やヘアスタイルで自由に楽しんで。
@@ -218,11 +252,11 @@ Template Name: top
           <img src="<?php bloginfo('template_url'); ?>/assets/images/dryer_thumb1.png" alt="">
         </div>
         <dl class="p-top-feature-section__card-body">
-          <dt class="p-top-feature-section__card-title c-text--30">
+          <dt class="p-top-feature-section__card-title c-txt--30">
             ヘアドライで<br>
             髪と頭皮を健やかにケア
           </dt>
-          <dd class="p-top-feature-section__card-text c-text--20">
+          <dd class="p-top-feature-section__card-text c-txt--20">
             使うたびに髪と頭皮をやさしくケア。<br>
             あなた専属のサロンへ。
           </dd>
@@ -239,11 +273,11 @@ Template Name: top
           <img src="<?php bloginfo('template_url'); ?>/assets/images/dryer_thumb2.png" alt="">
         </div>
         <dl class="p-top-feature-section__card-body">
-          <dt class="p-top-feature-section__card-title c-text--30">
+          <dt class="p-top-feature-section__card-title c-txt--30">
             4つの風温モードと<br>
             ミネラルマイナスイオン
           </dt>
-          <dd class="p-top-feature-section__card-text c-text--20">
+          <dd class="p-top-feature-section__card-text c-txt--20">
             髪や頭皮の状態に合わせて選べる多機能なモードを搭載。
           <dd>
             <div class="p-top-feature-section__card-button c-button c-button--round">
@@ -258,11 +292,11 @@ Template Name: top
           <img src="<?php bloginfo('template_url'); ?>/assets/images/dryer_thumb3.png" alt="">
         </div>
         <dl class="p-top-feature-section__card-body">
-          <dt class="p-top-feature-section__card-title c-text--30">
+          <dt class="p-top-feature-section__card-title c-txt--30">
             富士山溶岩配合ノズル&<br>
             オールインワンセット
           </dt>
-          <dd class="p-top-feature-section__card-text c-text--20">
+          <dd class="p-top-feature-section__card-text c-txt--20">
             自然の力を利用して今までにないサラサラのツヤ髪を作り出します。
           <dd>
             <div class="p-top-feature-section__card-button c-button c-button--round">
@@ -277,11 +311,11 @@ Template Name: top
           <img src="<?php bloginfo('template_url'); ?>/assets/images/dryer_thumb4.png" alt="">
         </div>
         <dl class="p-top-feature-section__card-body">
-          <dt class="p-top-feature-section__card-title c-text--30">
+          <dt class="p-top-feature-section__card-title c-txt--30">
             スペック＆<br>
             安心のまごころ保証
           </dt>
-          <dd class="p-top-feature-section__card-text c-text--20">
+          <dd class="p-top-feature-section__card-text c-txt--20">
             万が一のトラブルに備えた安心保証を全てのお客様にご提供いたします。
           <dd>
             <div class="p-top-feature-section__card-button c-button c-button--round">
@@ -306,7 +340,7 @@ Template Name: top
               試す・体感する。<br>
               お取り扱い店舗一覧。
             </h2>
-            <p class="p-top-kv-section__card-txt c-text--20">
+            <p class="p-top-kv-section__card-txt c-txt--20">
               エコアラドライヤーは弊社に認定された<br>
               サロン・ホテルからのみ購入できます。<br>
               是非、お近くのサロンでお試しください。
@@ -333,7 +367,7 @@ Template Name: top
               お取り扱い<br>
               認定店になるために。
             </h2>
-            <p class="p-top-kv-section__card-txt c-text--20">
+            <p class="p-top-kv-section__card-txt c-txt--20">
               弊社にお取扱店として認定されますと<br>
               お客様にドライヤーをご紹介いただけます。<br>
               詳しくはお問い合わせください。
@@ -352,51 +386,36 @@ Template Name: top
     <h1 class="p-top-topics__heading c-heading-section--large c-heading-section--center">
       TOPICS
     </h1>
+    <?php
+    $args = [
+      'post_type' => 'post',
+      'posts_per_page' => 4
+    ];
+    $the_query = new WP_Query($args);
+    ?>
     <div class="p-top-topics__posts-list">
-      <dl class="p-top-topics__posts-list-item c-posts-list-card">
-        <dd class="c-posts-list__thumbnail">
-          <img src="<?php bloginfo('template_url'); ?>/assets/images/thumb.png" alt="">
-        </dd>
-        <dd class="p-top-topics__posts-list-date c-posts-list-card__date">
-          <time>2022/00/00</time>
-        </dd>
-        <dt class="p-top-topics__posts-list-title c-posts-list-card__title">
-          記事タイトルダミー記事タイトルダミー記事タイトルダミー記事タイトルダミー
-        </dt>
-      </dl>
-      <dl class="p-top-topics__posts-list-item c-posts-list-card">
-        <dd class="c-posts-list__thumbnail">
-          <img src="<?php bloginfo('template_url'); ?>/assets/images/thumb.png" alt="">
-        </dd>
-        <dd class="p-top-topics__posts-list-date c-posts-list-card__date">
-          <time>2022/00/00</time>
-        </dd>
-        <dt class="p-top-topics__posts-list-title c-posts-list-card__title">
-          記事タイトルダミー記事タイトルダミー記事タイトルダミー記事タイトルダミー
-        </dt>
-      </dl>
-      <dl class="p-top-topics__posts-list-item c-posts-list-card">
-        <dd class="c-posts-list__thumbnail">
-          <img src="<?php bloginfo('template_url'); ?>/assets/images/thumb.png" alt="">
-        </dd>
-        <dd class="p-top-topics__posts-list-date c-posts-list-card__date">
-          <time>2022/00/00</time>
-        </dd>
-        <dt class="p-top-topics__posts-list-title c-posts-list-card__title">
-          記事タイトルダミー記事タイトルダミー記事タイトルダミー記事タイトルダミー
-        </dt>
-      </dl>
-      <dl class="p-top-topics__posts-list-item c-posts-list-card">
-        <dd class="c-posts-list__thumbnail">
-          <img src="<?php bloginfo('template_url'); ?>/assets/images/thumb.png" alt="">
-        </dd>
-        <dd class="p-top-topics__posts-list-date c-posts-list-card__date">
-          <time>2022/00/00</time>
-        </dd>
-        <dt class="p-top-topics__posts-list-title c-posts-list-card__title">
-          記事タイトルダミー記事タイトルダミー記事タイトルダミー記事タイトルダミー
-        </dt>
-      </dl>
+      <?php if ($the_query->have_posts()) : ?>
+        <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+          <dl class="p-top-topics__posts-list-item c-posts-list-card">
+            <dd class="c-posts-list__thumbnail">
+              <?php if (has_post_thumbnail()) : ?>
+                <?php the_post_thumbnail(); ?>
+              <?php else : ?>
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/thumb.png" alt="">
+              <?php endif; ?>
+            </dd>
+            <dd class="p-top-topics__posts-list-date c-posts-list-card__date">
+              <time datetime="<?php the_time('Y/m/d'); ?>"><?php the_time('Y/m/d'); ?></time>
+            </dd>
+            <dt class="p-top-topics__posts-list-title c-posts-list-card__title">
+              <?php the_title(); ?>
+            </dt>
+          </dl>
+        <?php endwhile;
+        wp_reset_postdata(); ?>
+      <?php else : ?>
+        <div class="c-txt--30 c-txt--center u-margin__x--auto">記事が見つかりませんでした。</div>
+      <?php endif; ?>
     </div>
     <div class="p-top-topics__button c-button c-button--round">
       <a href="#">Read More</a>
@@ -416,8 +435,8 @@ Template Name: top
         </div>
       </div>
       <div class="p-top-instagram__button c-button c-button--round">
-          <a href="#">Read More</a>
-        </div>
+        <a href="#">Read More</a>
+      </div>
     </div>
   </section>
 </main>
